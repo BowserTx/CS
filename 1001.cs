48215bfa2769481d9906248a0f419c13ece443a9
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2022-02-13 09:10:22 -0400

    URI

---
+++ 1001.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string number1 = Console.ReadLine();
+            string number2 = Console.ReadLine();
+
+            Console.WriteLine("X = {0}", Convert.ToInt32(number1) + Convert.ToInt32(number2));
+            Console.Read();
+
+    }
+
+}
---
+++ 1002.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+  double raio, area;
+             raio = Convert.ToDouble(Console.ReadLine());
+
+            area =  3.14159 * (raio * raio);
+            Console.WriteLine("A={0:0.0000}", area);
+            Console.Read();
+    }
+
+}
